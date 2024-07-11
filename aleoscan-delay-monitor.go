ca92351a174fb098651237885cd14f36cd0ecace
kunkka <kunkka@kunkka-MacBookPro-work.local>
2024-07-11 16:12:34 +0800

    add product

--- aleoscan-delay-monitor.go
+++ aleoscan-delay-monitor.go
@@ -11,12 +11,13 @@ import (
 var pgDSN = flag.String("pgdsn", "", "Postgres connection string,example:postgres://postgres:123456@localhost:5432/postgres?sslmode=disable")
 var Interval = flag.Int("interval", 1, "Interval in minutes to run the monitor")
 var pushAddr = flag.String("push-address", "", "Address of the Pushgateway to send metrics,example:http://127.0.0.1:9091")
+var product = flag.String("product", "", "Product name")
 
 func main() {
 	flag.Parse()
 
-	if *pushAddr == "" || *pgDSN == "" {
-		log.Fatalln("push-address and pgdsn must be provided")
+	if *pushAddr == "" || *pgDSN == "" || *product == "" {
+		log.Fatalln("push-address、pgdsn、product must be provided")
 	}
 
 	db, err := sqlexec.InitDB(*pgDSN)
@@ -31,10 +32,10 @@ func main() {
 			log.Println(err)
 			time.Sleep(time.Duration(*Interval) * time.Minute)
 		}
-		delay:= time.Now().Unix() - ts
+		delay := time.Now().Unix() - ts
 		log.Printf("height:%d, ts:%d,delay:%d", height, ts, delay)
-		prometh.Push(*pushAddr, "aleoscan_cur_height", height)
-		prometh.Push(*pushAddr, "aleoscan_cur_timestamp_delay_second",delay)
+		prometh.Push(*pushAddr, *product+"_cur_height", height)
+		prometh.Push(*pushAddr, *product+"_cur_timestamp_delay_second", delay)
 
 		time.Sleep(time.Duration(*Interval) * time.Minute)
 	}
